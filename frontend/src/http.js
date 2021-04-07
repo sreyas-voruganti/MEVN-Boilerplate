e0428c709612e8ef64bde2bcc431547e54d5f0bb
Sreyas Voruganti <sreyas.voruganti@gmail.com>
2021-04-07 13:31:51 -0400

    describe env vars

--- frontend/src/http.js
+++ frontend/src/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: 'http://localhost:8000'
+  baseURL: 'http://localhost:8000' // The base URL for your api
 })
 
 export default http
